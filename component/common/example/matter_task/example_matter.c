4d12f5c4631f21e52111c3039355c47a706e45c9
mikewen <zihxiangwen@gmail.com>
2021-12-16 15:48:49 +0800

    Modifications for meross

--- component/common/example/matter_task/example_matter.c
+++ component/common/example/matter_task/example_matter.c
@@ -4,8 +4,14 @@
 #include "basic_types.h"
 #include "platform_opts.h"
 #include "section_config.h"
+#include "gpio_api.h"
+#include "gpio_irq_api.h"
 
 #if defined(CONFIG_EXAMPLE_MATTER) && CONFIG_EXAMPLE_MATTER
+
+#define GPIO_IRQ_PIN        PA_19
+
+gpio_irq_t gpio_btn;
 
 extern void ChipTest(void);
 
@@ -19,8 +25,25 @@ static void example_matter_task_thread(void *pvParameters)
     return;
 }
 
+void gpio_demo_irq_handler(uint32_t id, gpio_irq_event event)
+{
+#if CONFIG_EXAMPLE_WLAN_FAST_CONNECT
+    Erase_Fastconnect_data();
+    printf("Erased Fast Connect data\r\n");
+#endif
+
+}
+
+void init_irq(void)
+{
+    gpio_irq_init(&gpio_btn, GPIO_IRQ_PIN, gpio_demo_irq_handler, NULL);
+    gpio_irq_set(&gpio_btn, IRQ_FALL, 1);    // Falling Edge Trigger
+    gpio_irq_enable(&gpio_btn);
+}
+
 void example_matter_task(void)
 {
+    init_irq();
     if(xTaskCreate(example_matter_task_thread, ((const char*)"example_matter_task_thread"), 8192, NULL, tskIDLE_PRIORITY + 1, NULL) != pdPASS)
         printf("\n\r%s xTaskCreate(example_matter_task_thread) failed", __FUNCTION__);
 }
